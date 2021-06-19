244a35bbd27020bd0b0fa598b9f2d0a452600724
brcre001 <brandon.f.creed@gmail.com>
2021-06-19 00:42:54 +0000

    Finished exercises with JEST

---
+++ app.js
@@ -0,0 +1,36 @@
+// app.js file content
+
+// this is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// just a console log for ourselves.
+console.log(sum(7,3))
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 110.24
+    return valueInYen
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.0065
+    return valueInPound
+}
+
+// we include fromEuroToDollar here as well because it needs to be exported
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
